db2c386ccffc63ba228d6451d1f028753180ec77
ShreyaPrabhu <shreyaprabhu96@gmail.com>
2016-03-20 17:50:01 +0530

    add motivations

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var motivations = require('motivations');
+
 
 app.get('/', function(request, response){
 	
@@ -11,4 +13,8 @@ app.get('/Hello', function(request, response){
 	response.send("NodeTogether is cool");
 });
 
+app.get('/Mot',function(request, response){
+	
+	response.send(motivations[6]);
+});
 module.exports = app;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  var host = '0.0.0.0'; //says i am using localhost
  
  var express = require('express'); //includes packages
+  
  var app = express();
  
  app.use(require('./app/index.js'))
