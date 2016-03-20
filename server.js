ee14061362b7978fa9d887e526f7006d546ffffd
ShreyaPrabhu <shreyaprabhu96@gmail.com>
2016-03-20 17:07:16 +0530

    Add server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+ var port = '8080';  //mentioning port
+ var host = '0.0.0.0'; //says i am using localhost
+ 
+ var express = require('express'); //includes packages
+ var app = express();
+ 
+ app.listen(port,host);
+ console.log("Server is listening...");
+ 
